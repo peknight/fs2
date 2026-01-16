37b342f85b4443a2db58318a62905ca70607ce7b
peknight <JKpeknight@gmail.com>
2026-01-16 09:33:04 +0800

    update

--- fs2-io/jvm/src/main/scala/com/peknight/fs2/io/syntax/PathSyntax.scala
+++ fs2-io/jvm/src/main/scala/com/peknight/fs2/io/syntax/PathSyntax.scala
@@ -1,9 +1,11 @@
 package com.peknight.fs2.io.syntax
 
-import cats.Applicative
 import cats.effect.Sync
 import cats.syntax.applicative.*
-import fs2.io.file.{Files, Path}
+import cats.syntax.flatMap.*
+import cats.syntax.functor.*
+import cats.{Applicative, Monad}
+import fs2.io.file.{Files, Path, Permissions}
 
 import java.io.{FileInputStream, FileOutputStream, FileReader, FileWriter}
 import java.nio.file.Paths
@@ -16,8 +18,15 @@ trait PathSyntax:
       Sync[F].blocking(new FileInputStream(path.toNioPath.toFile))
     def toFileOutputStream[F[_]: Sync]: F[FileOutputStream] =
       Sync[F].blocking(new FileOutputStream(path.toNioPath.toFile))
-    def createParentDirectories[F[_]: {Applicative, Files}]: F[Unit] =
-      path.parent.fold(().pure[F])(parent => Files[F].createDirectories(parent))
+    def createParentDirectories[F[_]: {Applicative, Files}](permissions: Option[Permissions] = None): F[Unit] =
+      path.parent.fold(().pure[F])(parent => Files[F].createDirectories(parent, permissions))
+    def createFileIfNotExists[F[_]: {Monad, Files}](directoryPermissions: Option[Permissions] = None,
+                                                    filePermissions: Option[Permissions] = None): F[Unit] =
+      for
+        _ <- createParentDirectories[F](directoryPermissions)
+        _ <- Monad[F].ifM[Unit](Files[F].exists(path))(().pure[F], Files[F].createFile(path, filePermissions))
+      yield
+        ()
   end extension
   extension (path: Path.type)
     def fromResource[F[_]: Sync](resourcePath: String): F[Path] =
