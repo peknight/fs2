31b73a9a1b9083c000f445bcb72cc871fb8f5c5b
peknight <JKpeknight@gmail.com>
2026-05-19 14:09:06 +0800

    update

--- fs2-core/shared/src/main/scala/com/peknight/fs2/syntax/StreamSyntax.scala
+++ fs2-core/shared/src/main/scala/com/peknight/fs2/syntax/StreamSyntax.scala
@@ -1,9 +1,19 @@
 package com.peknight.fs2.syntax
 
+import cats.effect.*
+import cats.effect.std.AtomicCell
+import cats.effect.syntax.spawn.*
+import cats.syntax.applicative.*
+import cats.syntax.applicativeError.*
+import cats.syntax.apply.*
+import cats.syntax.flatMap.*
 import cats.syntax.functor.*
+import cats.syntax.option.*
 import cats.{Applicative, Functor}
 import com.peknight.fs2.pipe
 import fs2.{Chunk, Compiler, Pull, Stream}
+
+import java.util.NoSuchElementException
 
 trait StreamSyntax:
   extension [F[_], I] (stream: Stream[F, I])
@@ -50,6 +60,31 @@ trait StreamSyntax:
 
     def evalTapLast[F2[x] >: F[x] : Applicative, I2 >: I, O2](f: I2 => F2[O2]): Stream[F2, I] =
       stream.through(pipe.evalTapLast(f))
+
+    def resource[R](f: I => Resource[F, R])(using F: Concurrent[F]): Resource[F, Ref[F, R]] =
+      Resource.make(stream.uncons1.flatMap {
+        case Some((head, tail)) =>
+          for
+            (resource, release) <- f(head).allocated
+            ref <- Ref.of[F, R](resource)
+            // 外部将资源关闭时会将releaseCell置为None
+            releaseCell <- AtomicCell[F].of[Option[F[Unit]]](release.some)
+            fiber <- tail
+              .evalMap(a => releaseCell.evalModify {
+                case Some(release) => release *> f(a).allocated.map((nextResource, nextRelease) => (nextRelease.some, nextResource.some))
+                case finalized => (finalized, none[R]).pure[F]
+              }.flatMap(_.fold(().pure[F])(ref.set)))
+              .compile.drain.start
+          yield
+            (ref, releaseCell, fiber)
+        case _ => NoSuchElementException("empty stream")
+          .raiseError[F, (Ref[F, R], AtomicCell[F, Option[F[Unit]]], Fiber[F, Throwable, Unit])]
+      }) {
+        case (ref, releaseCell, fiber) => releaseCell.evalUpdate {
+          case Some(release) => release.as(none[F[Unit]])
+          case finalized => finalized.pure[F]
+        } *> fiber.cancel
+      }.map(_._1)
   end extension
 end StreamSyntax
 object StreamSyntax extends StreamSyntax
