d9fff707a8bef8bb038cf78ffc5c5b7764d11984
peknight <JKpeknight@gmail.com>
2026-05-19 17:00:43 +0800

    update

--- fs2-core/shared/src/main/scala/com/peknight/fs2/stream/package.scala
+++ fs2-core/shared/src/main/scala/com/peknight/fs2/stream/package.scala
@@ -4,18 +4,17 @@ import _root_.fs2.{Pull, Stream}
 import cats.Monad
 import cats.effect.kernel.Temporal
 import cats.syntax.either.*
-import cats.syntax.option.*
 
 import scala.concurrent.duration.*
 
 package object stream:
-  def unfoldTemporal[F[_]: Temporal, A](f: Option[A] => F[(Option[A], Option[FiniteDuration])]): Stream[F, A] =
-    def go: Pull[F, A, Unit] =
-      Monad[[X] =>> Pull[F, A, X]].tailRecM[Option[A], Unit](None)(opt => Pull.eval(f(opt)).flatMap {
-        case (Some(a), Some(duration)) => Pull.output1[F, A](a).flatMap(_ => Pull.sleep(duration)).as(a.some.asLeft)
-        case (Some(a), _) => Pull.output1[F, A](a).as(().asRight[Option[A]])
-        case (_, Some(duration)) => Pull.sleep(duration).as(opt.asLeft)
-        case _ => Pull.pure(().asRight[Option[A]])
+  def unfoldTemporal[F[_]: Temporal, S, O](s: S)(f: S => F[(Option[(O, S)], Option[FiniteDuration])]): Stream[F, O] =
+    def go: Pull[F, O, Unit] =
+      Monad[[X] =>> Pull[F, O, X]].tailRecM[S, Unit](s)(s => Pull.eval(f(s)).flatMap {
+        case (Some((o, nextS)), Some(duration)) => Pull.output1[F, O](o).flatMap(_ => Pull.sleep(duration)).as(nextS.asLeft)
+        case (Some((o, _)), _) => Pull.output1[F, O](o).as(().asRight[S])
+        case (_, Some(duration)) => Pull.sleep(duration).as(s.asLeft)
+        case _ => Pull.pure(().asRight[S])
       })
     go.stream
 end stream
